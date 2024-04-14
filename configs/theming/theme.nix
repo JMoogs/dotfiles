00f75ec705f5cf40cf31b42dd142e6b1b0c4f3ee
Jeremy S <Jeremy@jsmoog.com>
2024-04-14 17:56:43 +0100

    Added hyprlock

--- configs/theming/theme.nix
+++ configs/theming/theme.nix
@@ -2,6 +2,8 @@
 
 let theme = userOptions.theme; in {
 
+  name = theme;
+
   gtkTheme = if theme == "dracula" then {
     name = theme;
     package = pkgs.dracula-theme;
@@ -43,7 +45,9 @@ let theme = userOptions.theme; in {
   hyprTheme = if theme == "dracula" then {
     base = "rgb(282a36)";
     text = "rgb(f8f8f2)";
+    textAlpha = "f8f8f2";
     accent = "rgb(ffb86c)"; # Using dracula orange
+    accentAlpha = "ffb86c";
     surface0 = "rgb(44475a)";
     red = "rgb(ff5555)";
     yellow = "rgb(f1fa8c)";
@@ -51,7 +55,9 @@ let theme = userOptions.theme; in {
   else if theme == "frappe" then {
     base = "rgb(303446)";
     text = "rgb(c6d0f5)";
+    textAlpha = "c6d0f5";
     accent = "rgb(ca9ee6)"; # Mauve in catppuccin
+    accentAlpha = "ca9ee6";
     surface0 = "rgb(414559)";
     red = "rgb(e78284)";
     yellow = "rgb(e5c890)";
@@ -59,11 +65,18 @@ let theme = userOptions.theme; in {
   else if theme == "latte" then {
     base = "rgb(eff1f5)";
     text = "rgb(4c4f69)";
+    textAlpha = "4c4f69";
     accent = "rgb(8839ef)"; # Mauve in catppuccin
+    accentAlpha = "8839ef";
     surface0 = "rgb(ccd0da)";
     red = "rgb(d20f39)";
     yellow = "rgb(df8e1d)";
   }
+  else null;
+
+  hyprlockWallpaper = if theme == "dracula" then "/etc/nixos/wallpapers/dracula/lockscreen.png"
+  else if theme == "frappe" then "/etc/nixos/wallpapers/frappe/lockscreen.png"
+  else if theme == "latte" then "/etc/nixos/wallpapers/latte/lockscreen.png"
   else null;
 
    
